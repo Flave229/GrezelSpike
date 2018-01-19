f71bec7628fe8563954538b397aca83a185f1751
David Smith <david@flave.co.uk>
2018-01-19 16:26:50 +0000

    Adding player movement

---
+++ Assets/Scripts/InputScripts/ControlMapping.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.InputScripts
+{
+    public enum Control
+    {
+        MovePlayerRight,
+        MovePlayerLeft,
+        PlayerJump
+    }
+
+    public class ControlMapping
+    {
+        private static ControlMapping _instance;
+        private static Dictionary<Control, string> _mappedControls;
+
+        private ControlMapping()
+        {
+            _mappedControls = new Dictionary<Control, string>
+            {
+                { Control.MovePlayerLeft, "left" },
+                { Control.MovePlayerRight, "right" },
+                { Control.PlayerJump, "up" }
+            };
+        }
+
+        public static ControlMapping Instance()
+        {
+            return _instance ?? (_instance = new ControlMapping());
+        }
+
+        public bool GetControlPressed(Control control)
+        {
+            string mappedKey = _mappedControls[control];
+
+            if (Input.GetKey(mappedKey))
+            {
+                Debug.Log("Key Down! " + mappedKey);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.InputScripts;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        private ControlMapping _controlMappings;
+
+        void Start ()
+        {
+            _controlMappings = ControlMapping.Instance();
+        }
+
+        void Update ()
+        {
+            if (_controlMappings.GetControlPressed(Control.MovePlayerLeft))
+                transform.position = new Vector3(transform.position.x - 0.01f, transform.position.y, transform.position.z);
+            if (_controlMappings.GetControlPressed(Control.MovePlayerRight))
+                transform.position = new Vector3(transform.position.x + 0.01f, transform.position.y, transform.position.z);
+        }
+    }
+}
